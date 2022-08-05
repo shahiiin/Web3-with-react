ab026b410772b8fa7cc068e9ea363877a58d2494
shahin <shahin.shahi1986@gmail.com>
2022-08-05 14:51:36 +0300

    balance component is finished

--- components/Balance.jsx
+++ components/Balance.jsx
@@ -1,7 +1,7 @@
-import { Text } from '@chakra-ui/react'
+import { Divider, Text } from '@chakra-ui/react'
 import Moralis from 'moralis'
 import { useEffect, useState } from 'react'
-import { useMoralisWeb3Api } from 'react-moralis'
+import { useERC20Balances, useMoralisWeb3Api } from 'react-moralis'
 import CustomContainer from './CustomContainer'
 
 
@@ -10,6 +10,8 @@ export default function Balance({ user }) {
     const [ethBalance, SetEthBalance] = useState(0)
 
     const Web3Api = useMoralisWeb3Api()
+
+    const { fetchERC20Balances, data } = useERC20Balances()
 
     const fetchNativeBalance = async () => {
         const result = await Web3Api.account.getNativeBalance({
@@ -22,13 +24,25 @@ export default function Balance({ user }) {
     }
     useEffect(() => {
         fetchNativeBalance()
+        fetchERC20Balances({
+            params: {
+                chain: "rinkeby",
+                address: user.get('ethAddress')
+            }
+        })
     }, [])
-
+    console.log(data, 'aa')
     return (
 
         <CustomContainer>
-            <Text>My ERC20 Tokens</Text>
+            <Text mb="6"fontSize="xl" fontWeight="bold">My ERC20 Tokens</Text>
             {ethBalance && <Text>💰{ethBalance}ETH</Text>}
+            <Divider />
+            {data && data.map(token => (
+                <div key={token.symbol}>
+                    <Text>💰{Moralis.Units.FromWei(token.balance)}<b>{token.symbol}</b></Text>
+                </div>
+            ))}
         </CustomContainer>
     )
 }
