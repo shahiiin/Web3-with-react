9b9a6f98b718c9be3e563d4d66a55021721bd74a
shahin <shahin.shahi1986@gmail.com>
2022-08-05 17:32:47 +0300

    make on change function in form

--- components/Send.jsx
+++ components/Send.jsx
@@ -1,16 +1,27 @@
-import { FormControl, FormLabel, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Text } from "@chakra-ui/react";
+import { Button, FormControl, FormLabel, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Text } from "@chakra-ui/react";
+import Moralis from "moralis/types";
 import { useState } from "react";
+import { useWeb3Transfer } from "react-moralis";
 import CustomContainer from "./CustomContainer";
 
 export default function Send() {
 
     const [amount, setAmount] = useState(0)
     const handleChange = (value) => setAmount(value)
+    const [receiver, setReseiver] = useState()
+    const { fetch, isFetching } = useWeb3Transfer({
+        amount: Moralis.Units.ETH(amount),
+        receiver: receiver,
+        type: "native"
+    })
+
 
     return (
         <CustomContainer>
             <Text fontSize="xl" fontWeight="bold">Send ETH</Text>
-            <form>
+            <form onSubmit={e => {
+                e.preventDefault()
+            }}>
                 <FormControl mt="4">
                     <FormLabel htmlFor="amount">
                         Amount of ETH
@@ -22,7 +33,12 @@ export default function Send() {
                             <NumberDecrementStepper />
                         </NumberInputStepper>
                     </NumberInput>
+                    <FormLabel mt="4" htmlFor="receiver">
+                        Send to
+                    </FormLabel>
+                    <Input id="receiver" type="text" placeholder="Receiver address" value={receiver} onChange={e => setReseiver(e.target.value)} />
                 </FormControl>
+                <Button mt="4" type="submit" colorScheme="purple">Send</Button>
             </form>
         </CustomContainer>
     )
